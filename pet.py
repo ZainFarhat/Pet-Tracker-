32bb1bf253f4aa073e5195bb8efb27ca2046f9ae
farhatzain <zain.farhat@programi.tech>
2025-07-01 16:41:23 +0300

    Creating a method to prints all pet data

--- pet.py
+++ pet.py
@@ -1,9 +1,12 @@
 # Create a Pet class
 class Pet:
-    # Constructor that initializes name, species, age, adopted 
+    #Constructor that initializes name, species, age, adopted 
     def __init__(self, name, species, age, adopted=False):
         self.name = name
         self.species = species
         self.age = age
         self.adopted = adopted
 
+# Method to display_info() → prints all pet data in a readable sentence.
+    def display_info(self):
+        print(f"{self.name} is a {self.age}-year-old {self.species}. Adopted: {self.adopted}")
